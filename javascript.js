27edb7b0ed2f8e1ae59dd8c1280939a01c7769f8
caleb <calebtelfe@gmail.com>
2025-06-13 13:51:17 -0400

    jotting down info

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// Board constraints
+/*
+        as x,y
+[0,7] --------- [7,7]
+
+
+
+[0,0] --------- [7,0]
+
+* negative values are out of bounds.
+* x and y can not be greater than 7 or its out of bounds.
+*/
+
+
+/* Knight Moves 
+
+8 possible moves
+
+Right side moves
+(1) x+2, y + 1;
+(2) x+2,  y-1;
+(3) x+1, y + 2;
+(4) x+1, y - 2;
+
+Left Side moves
+(1) x-2, y + 1;
+(2) x-2,  y-1;
+(3) x-1, y + 2;
+(4) x-1, y - 2;
+
+*/
